15dd0174d3596b75a71f088e3314c99627445853
Martin Rubio <rubio.martin00@gmail.com>
2025-05-29 22:27:43 +0200

    basic fetch and display copied

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
     </>
   );
 }
