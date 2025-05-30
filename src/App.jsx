1ad25b24f1810ee884b659255e8a0f119330f0d7
Martin Rubio <rubio.martin00@gmail.com>
2025-05-30 23:07:29 +0200

    treating json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import { Footer } from './assets/Footer';
 import { Header } from './assets/Header';
 import { Main } from './assets/Main';
+import './App.css'
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
     <>
       <Header />
--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,7 +1,29 @@
 import './Main.css'
+import { useState, useEffect } from 'react'
+
 
 export function Main() {
+    const [data, setData] = useState(null);
+
+    function clickProcess() {
+        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(response => response.json())
+        .then(json => setData(json))
+        .catch(error => console.error(error));
+    }
+
     return (
-        <div className='mainBox'>text</div>
+        <div className='mainBox'>
+            <div className='searchBox'>
+                <input className='searchBar' type="text" placeholder='Name of a pokemon...'/>
+                <button className='searchButton' onClick={clickProcess}>Search</button>
+            </div>
+            <div className='pokemonCard'>
+                <p>ID:</p>
+                <p>Name:</p>
+                <p>Type:</p>
+                <p>Image:</p>
+            </div>
+        </div>
     )
 }
